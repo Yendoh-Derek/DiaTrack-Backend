8f7d890ac9c41fd80e1869913751b7490c48fc19
Yendoh Derek <derek91t@gmail.com>
2025-06-09 02:58:52 +0000

    Updated CORS for local host

--- app/main.py
+++ app/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:59416",  # Flutter local dev
+        "http://localhost:63112",
         # Add other allowed origins as needed
         # "http://localhost:3000",  # React local dev (optional)
         # "https://your-frontend-domain.com"  # Production frontend (optional)
